009c2cbbdb7ca717e6339ce03a651faafa7d2660
Ara Stepanyan <arastepa@c1r7s6.42yerevan.am>
2021-02-28 15:48:41 +0400

    server

--- ft_server/srcs/init.sh
+++ ft_server/srcs/init.sh
@@ -6,3 +6,5 @@ echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILIGIES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 echo "update mysql.user set plugin = '' where user = 'root';" | mysql -u root --skip-password 
+
+bash
