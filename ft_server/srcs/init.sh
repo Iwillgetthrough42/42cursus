06b9682f7f2dcbe8b86dee53a3c828e4c805f11e
Ara Stepanyan <arastepa@c1r5s7.42yerevan.am>
2021-03-02 16:01:26 +0400

    server

--- ft_server/srcs/init.sh
+++ ft_server/srcs/init.sh
@@ -2,9 +2,9 @@ service nginx start
 service mysql start
 service php7.3-fpm start
 
-echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILIGIES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
-echo "update mysql.user set plugin = '' where user = 'root';" | mysql -u root --skip-password 
+echo "CREATE DATABASE wordpress;" | mysql -u root
+echo "GRANT All PRIVILEGES ON wordpress.* TO 'root'@'localhost';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+echo "update mysql.user set plugin = '' where user = 'root';" | mysql -u root
 
 bash
