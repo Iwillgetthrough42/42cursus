2506574925df19b96192bd2b6b04c8e03077a02a
arastepa <77893589+arastepa@users.noreply.github.com>
2022-06-15 18:17:38 +0400

    Delete ince directory

--- ince/srcs/requirements/mariadb/install.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE wordpress;
-CREATE USER 'arastepa'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'arastepa'@'%';
-FLUSH PRIVILEGES;
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
