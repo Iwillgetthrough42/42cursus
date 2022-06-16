3b09cf1299d4176310cceaaca1af77663184829d
arastepa <77893589+arastepa@users.noreply.github.com>
2022-06-16 18:35:27 +0400

    Add files via upload

--- incep/srcs/requirements/wordpress/tools/script.sh
+++ incep/srcs/requirements/wordpress/tools/script.sh
@@ -1,10 +1,6 @@
-if [ -f "wp-config.php" ]; then
-    sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" wp-config.php
-    sed -i "s/MYSQL_USER/$MYSQL_USER/g" wp-config.php
-    sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" wp-config.php
-    mv wp-config.php /var/www/wordpress/
-    wp core install --url=${DOMAIN_NAME} --title="wordpress" --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=${EMAIL} --allow-root --path=/var/www/wordpress/
-fi
+wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --dbprefix=wp_ --allow-root --path=/var/www/wordpress/
+wp core install --url=${DOMAIN_NAME} --title="wordpress" --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=${EMAIL} --allow-root --path=/var/www/wordpress/
+
 
 mkdir -p /run/php
 
