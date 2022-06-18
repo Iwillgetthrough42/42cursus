104c0baaa4e57de35a3e4a539732ddbe1263b06d
arastepa <77893589+arastepa@users.noreply.github.com>
2022-06-18 17:27:51 +0400

    Add files via upload

--- incep/srcs/requirements/mariadb/tools/script.sh
+++ incep/srcs/requirements/mariadb/tools/script.sh
@@ -1,19 +1,7 @@
 #!/bin/bash	
 
-if [ -f install.sql ]; then
-
 sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" install.sql
 sed -i "s/MYSQL_USER/$MYSQL_USER/g" install.sql
 sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" install.sql
 sed -i "s/MYSQL_SUPER_USER/$MYSQL_SUPER_USER/g" install.sql
 sed -i "s/MYSQL_SUPER_PASSWORD/$MYSQL_SUPER_PASSWORD/g" install.sql
-
-
-service mysql start
-mysql -u root < install.sql
-service mysql stop
-rm -f install.sql
-
-fi
-
-exec "$@"
