2964c7a18a9afd04873381976dcc679eeb303c08
arastepa <77893589+arastepa@users.noreply.github.com>
2022-08-01 22:50:37 +0400

    Add files via upload

--- ft_containers/iterators/random_access_iterator.hpp
+++ ft_containers/iterators/random_access_iterator.hpp
@@ -1,6 +1,7 @@
 #ifndef RANDOM_ACCESS_ITERATOR_HPP
 # define RANDOM_ACCESS_ITERATOR_HPP
 #include "iterator.hpp"
+#include <iostream>
 
 namespace ft
 {
@@ -8,11 +9,11 @@ namespace ft
     class random_access_iterator : public ft::iterator<ft::random_access_iterator_tag, T>
     {
         public:
-            typedef ft::iterator<ft::random_access_iterator_tag, T>::iterator_category iterator_category;
-            typedef ft::iterator<ft::random_access_iterator_tag, T>::value_type value_type;
-            typedef ft::iterator<ft::random_access_iterator_tag, T>::difference_type difference_type;
-            typedef ft::iterator<ft::random_access_iterator_tag, T>::pointer pointer;
-            typedef ft::iterator<ft::random_access_iterator_tag, T>::reference reference;
+            typedef typename ft::iterator<ft::random_access_iterator_tag, T>::iterator_category iterator_category;
+            typedef typename ft::iterator<ft::random_access_iterator_tag, T>::value_type value_type;
+            typedef typename ft::iterator<ft::random_access_iterator_tag, T>::difference_type difference_type;
+            typedef typename ft::iterator<ft::random_access_iterator_tag, T>::pointer pointer;
+            typedef typename ft::iterator<ft::random_access_iterator_tag, T>::reference reference;
 
             random_access_iterator(pointer p = NULL)
             {
@@ -22,12 +23,18 @@ namespace ft
             {
                 this->p = other.p;
             }
+            operator random_access_iterator<const T> () const
+            {
+                std::cout << "hi" << std::endl;
+                return (random_access_iterator<const T>(this->p));
+            }
             random_access_iterator &operator=(const random_access_iterator &other)
             {
                 this->p = other.p;
+                return (*this);
             }
-            reference operator*(){return (*p)}
-            pointer operator->(){return (p)}
+            reference operator*(){return (*p);}
+            pointer operator->(){return (p);}
             random_access_iterator &operator++()
             {
                 this->p++;
@@ -64,7 +71,7 @@ namespace ft
             }
             reference operator[](size_t n){return (this->p[n]);}
             random_access_iterator &operator+=(size_t n){this->p += n; return (*this);}
-            random_access_iterator &operator-=(size_t n){this->p -= n; return (*this)}
+            random_access_iterator &operator-=(size_t n){this->p -= n; return (*this);}
             bool operator<(random_access_iterator rhs)
             {
                 return (this->p < rhs.p);
@@ -81,15 +88,13 @@ namespace ft
             {
                 return (this->p >= rhs.p);
             }
-            friend difference_type operator+(size_t n, random_access_iterator &rhs);
+            friend difference_type operator+(size_t n, random_access_iterator &rhs)
+            {
+                return (rhs.p += n);
+            }
         protected:
             pointer p;
     };
-    template <class T>
-    typename random_access_iterator<T>::difference_type operator+(size_t n, random_access_iterator<T> &rhs)
-    {
-        return (rhs.p += n);
-    }
 
 }
 
