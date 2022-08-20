8299bdc0f7941c57e06434bd86646acf24d66c81
arastepa <77893589+arastepa@users.noreply.github.com>
2022-08-20 19:22:25 +0400

    Add files via upload

--- ft_containers/containers/map.hpp
+++ ft_containers/containers/map.hpp
@@ -18,6 +18,7 @@ namespace ft
             typedef typename ft::pair<const key_type, mapped_type> value_type;
             typedef Compare key_compare;
             typedef Alloc allocator_type;
+            typedef Node<const key_type, mapped_type> node;
             class value_compare:std::binary_function<value_type,value_type,bool>
             {  
                 friend class map;
@@ -55,10 +56,140 @@ namespace ft
             typedef typename Alloc::template rebind<Node<const key_type, mapped_type>>::other node_alloc_type;
             size_type _size;
             key_compare _compare;
+            value_compare _compval;
             node_alloc_type _node_alloc;
-            Node<key_type, value_type>* _root;
+            node* _root;
+            node* _nil;
             allocator_type _alloc;
 
+            void left_rotate(node *x)
+            {
+                node *y = x->right;
+                x->right = y->left;
+                if (y->left != _nil)
+                {
+                    y->left->parent = x;
+                }
+                y->parent = x->parent;
+                if (x->parent == _nil)
+                    _root = y;
+                else if (x == x->parent->left)
+                {
+                    x->parent->left = y;
+                }
+                else
+                {
+                    x->parent->right = y;
+                }
+                y->left = x;
+                x->parent = y;
+            }
+            void right_rotate(node *x)
+            {
+                node *y = x->left;
+                x->left = y->right;
+                if (y->right != _nil)
+                {
+                    y->right->parent = x;
+                }
+                y->parent = x->parent;
+                if (x->parent == _nil)
+                    _root = y;
+                else if (x == x->parent->right)
+                {
+                    x->parent->right = y;
+                }
+                else
+                {
+                    x->parent->left = y;
+                }
+                y->right = x;
+                x->parent = y;
+            }
+            node *_find(node *x, value_type val)
+            {
+               if (x == -nil || (!_compval(*(x->data), val) && !_compval(val, *(x->data))))
+               {
+                    return (x);
+               }
+               if (_compval(val, *(x->data)))
+                    return (_find(x->left, val));
+                else
+                    return (_find(x->right, val));
+            }
+            void _insert(node *z)
+            {
+                node *y = _nil;
+                node *x = _root;
+                while (x != _nil)
+                {
+                    y = x;
+                    if (_compval(*(z->data), *(x->data)))
+                        x = x->left;
+                    else
+                        x = x->right;
+                }
+                z->parent = y;
+                if (y == _nil)
+                {
+                    _root = z;
+                }
+                else if(_compval(*(z->data), *(y->data)))
+                    y->left = z;
+                else
+                    y->right = z;
+                z->left = _nil;
+                z->right = _nil;
+                z->color = RED;
+                _insert_fixup(z);
+            }
+        void insert_fixup(node *z)
+        {
+            node *y;
+
+            while (z->parent->color == RED)
+            {
+                if (z->parent = z->parent->parent->left)
+                {
+                    y = z->parent->parent->right;
+                    if (y->color == RED)
+                    {
+                        z->parent->color = BLACK;
+                        y->color = BLACK:
+                        z->parent->parent = color = RED;
+                        z = z->parent->parent;
+                    }
+                    else if (z == z->parent->right)
+                    {
+                        z = z->parent;
+                        left_rotate(z);
+                    }
+                    z->parent->color = BLACK:
+                    z->parent->parent->color = RED;
+                    right_rotate(z->parent->parent);
+                }
+                else
+                {
+                    y = z->parent->parent->left;
+                    if (y->color == RED)
+                    {
+                        z->parent->color = BLACK;
+                        y->color = BLACK:
+                        z->parent->parent = color = RED;
+                        z = z->parent->parent;
+                    }
+                    else if (z == z->parent->left)
+                    {
+                        z = z->parent;
+                        left_rotate(z);
+                    }
+                    z->parent->color = BLACK:
+                    z->parent->parent->color = RED;
+                    right_rotate(z->parent->parent);
+                }
+            }
+            _root.color = BLACK:
+        }
     };
     
 }
